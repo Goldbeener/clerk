6bdbbe63c36d5d5e5278486e0e858c7873cfa2e4
duxin <duxin@pbcdci.cn>
2023-04-08 10:06:44 +0800

    format

--- packages/renderer/types/mockjs/index.d.ts
+++ packages/renderer/types/mockjs/index.d.ts
@@ -1,15 +1,13 @@
-
-interface RandomIns  {
-  sentence: (min:number, max:number) => string,
-  csentence: (min:number, max:number) => string[],
-  id: () => string,
-  datetime: () => string,
-  [key: string]: () => string
+interface RandomIns {
+  sentence: (min: number, max: number) => string;
+  csentence: (min: number, max: number) => string[];
+  id: () => string;
+  datetime: () => string;
+  [key: string]: () => string;
 }
 
 declare module 'mockjs' {
   const Random: RandomIns;
   const mock: (any) => any[];
-  modulex.export = { Random, mock };
+  modulex.export = {Random, mock};
 }
-
